6776d285b625bf005f9037c4202a024d4ddfbd3e
PieStw <piero.stalteri@icloud.com>
2024-10-21 14:23:08 +0200

    esercizio 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function check(names, letter){
+    let newArray = [];
+    names.forEach(element => {
+        if(element[0] == letter)
+            newArray.push(element);
+    });
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const newArray = check(names, "A");
+console.log(newArray);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
