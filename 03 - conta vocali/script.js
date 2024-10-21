da922136ccb6745de46ebaaddb27396dd6eeb37c
PieStw <piero.stalteri@icloud.com>
2024-10-21 14:28:45 +0200

    esercizio 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,19 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function check(word){
+    let vocali = 0;
+    
+    for(let i = 0; i < word.length; i++){
+        if(word[i] == "a" || word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u")
+            vocali += 1;
+    }
+    return vocali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const vocali = check(word);
+console.log(vocali);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
